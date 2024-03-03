d4b235931ce25da4257252f863318a6b4bf75ba6
Peter <peteraninsky@yandex.ru>
2024-03-03 17:38:31 +0200

    fix last commit1

--- src/features/authByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/authByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import { classNames } from "shared/lib/classNames/classNames"
-import cls from './LoginModal.module.scss'
 import { Modal } from "shared/ui/Modal/Modal"
 import { LoginForm } from "../LoginForm/LoginForm"
 
@@ -11,7 +10,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, toggleModal }: LoginModalProps) => {
   return (
-    <Modal className={classNames(cls.loginModal, {}, [])}
+    <Modal className={classNames('', {}, [])}
       isOpen={isOpen}
       toggleModal={toggleModal}>
       <LoginForm />
